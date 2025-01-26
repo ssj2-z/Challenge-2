2e2659914c35f419e268a65e6f85d180bde85bd4
ssj2-z <ssj2@usf.edu>
2025-01-26 15:53:30 -0500

    Add files via upload

--- cc_2.js
+++ cc_2.js
@@ -25,3 +25,44 @@ let employee = {
 employee.department = "Sales";
 employee.position = "Team Leader";
 console.log("update employee records:" , employee);
+
+//Task 4
+let customers = [
+    {
+        name: "Michael Jordan",
+        email: "MJordan@gmail.com",
+        purchaseAmount: 23.00
+    },
+    {
+        name: "Scottie Pitman",
+        email: "PitmanS@gmail.com",
+        purchaseAmount: 8.25
+    },
+    {
+        name: "Michael Jackson",
+        email: "MJ@gmail.com",
+        purchaseAmount: 205.05
+    }
+];
+let newCustomer = {
+    name: "Taylor Parker",
+    email: "TParker@gmail.com",
+    purchaseAmount: 125.00
+};
+customers.push(newCustomer);
+console.log(customers);
+
+//Task 5
+let order = {
+    orderId: 13579,
+    customerName: "Ric Flair",
+    amount: 350.00
+    calculateTax: function () {
+        return this.amountt * .10;
+    }
+};
+console.log("Order Details:");
+console.log("Order ID:" order.orderId);
+console.log("Customer Name:" order.customerName);
+console.log("Order Amount:" order.amount);
+console.log("Tax Amount:" order.calculateTax());
